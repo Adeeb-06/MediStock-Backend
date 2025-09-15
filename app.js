70266d26aaa102110cabdceef0dbc0b9f431cc5c
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-09-15 20:06:30 +0600

    fa

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
 app.use(cors({
-  origin: `https://medi-stock-adeeb.vercel.app`,
+  origin: ['https://medi-stock-adeeb.vercel.app', 'http://localhost:3000'],
   credentials: true
 }));
 app.get('/', (req, res) => {
