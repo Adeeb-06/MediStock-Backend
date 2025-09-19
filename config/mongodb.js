fc76676297a0593a1e3eaa11ac03a3cd36a72731
Adeeb-06 <mohammadadeeb886@gmail.com>
2025-09-19 09:28:56 +0600

    errr

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
-const connectMongoDb = async () =>{
+const connectMongoDb = async () => {
     try {
-       let res = await mongoose.connect(process.env.MONGODB_DB_URI);
-       if(res){
-        console.log("MongoDB connected");
-       }
+        let res = await mongoose.connect(process.env.MONGODB_DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        if (res) {
+            console.log("MongoDB connected");
+        }
     } catch (error) {
         console.log('mongoDB connection error', error);
     }
